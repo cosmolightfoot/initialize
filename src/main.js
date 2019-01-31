7d01e7124f1ffa0ca1ec7eeb06007fa6db963f69
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-30 16:46:56 -0800

    finish project

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const coinPic = document.getElementById('coin-pic');
 const flipForm = document.getElementById('flip-form');
 const winNode = document.getElementById('wins');
 const lossNode = document.getElementById('losses');
+const betResult = document.getElementById('bet-result');
 
 flipForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -19,16 +20,19 @@ flipForm.addEventListener('submit', function(event) {
     else {
         coinPic.src = '../assets/coin-horse.jpg';
     }
+
+    coinPic.classList.remove('hidden');
     
     const bet = flipForm.elements.flip.value;
     if(bet === flipResult) {
         winCount++;
         winNode.textContent = winCount;
-
+        betResult.textContent = "You win!";
     }
     else {
         lossCount++;
         lossNode.textContent = lossCount;
+        betResult.textContent = "You lose!";
     }
 
     console.log(bet);
